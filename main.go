d85e8d0273c96ff024d813b552f48c6d596c74e3
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2024-02-23 18:20:38 +0600

    feat: finish lexer

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/Yakiyo/monkey/repl"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	os.Exit(0)
+	fmt.Println("Initializing Monkey repl")
+	fmt.Println()
+	repl.Start(os.Stdin, os.Stdout)
+	
 }
